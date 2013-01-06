37002d5e3b44befdafa9b486c9316001a9b87cf7
Rangel Reale <rangelspam@gmail.com>
2013-01-06 14:10:23 -0200

    * Linux implementation

--- include/GeneralDDC/Exceptions.h
+++ include/GeneralDDC/Exceptions.h
@@ -2,6 +2,7 @@
 #define H__GENERALDDC_EXCEPTIONS__H
 
 #include <stdexcept>
+#include <string>
 
 namespace GeneralDDC {
 
@@ -9,7 +10,12 @@ class Exception : public std::exception
 {
 public:
 	Exception() : std::exception() {}
-	Exception(const char * const &message) : std::exception(message) {}
+	Exception(const std::string &message) : std::exception(), _message(message) {}
+	~Exception() throw() {}
+	
+	const char* what() const throw() { return _message.c_str(); }
+private:
+	std::string _message;
 };
 
 };
